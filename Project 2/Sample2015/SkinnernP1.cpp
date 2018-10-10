6d0089ef08b86887e269cdbc6a5de4db1cd1548e
Skinner <Nick.Skinner@dnvgl.com>
2018-10-10 15:10:39 -0700

    Polygon Copter
    Also recorded video

--- Project 2/Sample2015/SkinnernP1.cpp
+++ Project 2/Sample2015/SkinnernP1.cpp
@@ -242,7 +242,7 @@ void	Axes( float );
 void	HsvRgb( float[3], float [3] );
 
 //constant global variable for transforming -10 z spaces because i'm lazy
-const int transZform = -10;
+const int transZform = -20;
 
 
 
@@ -328,6 +328,45 @@ Animate( )
 
 	//glutSwapBuffers();
 }
+
+
+
+float
+Dot(float v1[3], float v2[3])
+{
+	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
+void
+Cross(float v1[3], float v2[3], float vout[3])
+{
+	float tmp[3];
+	tmp[0] = v1[1] * v2[2] - v2[1] * v1[2];
+	tmp[1] = v2[0] * v1[2] - v1[0] * v2[2];
+	tmp[2] = v1[0] * v2[1] - v2[0] * v1[1];
+	vout[0] = tmp[0];
+	vout[1] = tmp[1];
+	vout[2] = tmp[2];
+}
+float
+Unit(float vin[3], float vout[3])
+{
+	float dist = vin[0] * vin[0] + vin[1] * vin[1] + vin[2] * vin[2];
+	if (dist > 0.0)
+	{
+		dist = sqrt(dist);
+		vout[0] = vin[0] / dist;
+		vout[1] = vin[1] / dist;
+		vout[2] = vin[2] / dist;
+	}
+	else
+	{
+		vout[0] = vin[0];
+		vout[1] = vin[1];
+		vout[2] = vin[2];
+	}
+	return dist;
+}
+
 
 
 // draw the complete scene:
@@ -419,7 +458,7 @@ Display( )
 	}
 
 	
-	cout << BladeAngle << " ";
+	//cout << BladeAngle << " ";
 
 	//glRotatef(BladeAngle, 0, 1, 0.);
 
@@ -1120,20 +1159,40 @@ InitLists( )
 	glColor4f(0, 1, 0, 0);
 	//**************************************************************************
 	int i;
-	struct edge *ep;
-	struct point *p0, *p1;
+	struct point *p0, *p1, *p2;
+	struct tri *tp;
+	float p01[3], p02[3], n[3];
 
 	glPushMatrix();
 	glTranslatef(0., -1., 0.);
 	glRotatef(97., 0., 1., 0.);
 	glRotatef(-15., 0., 0., 1.);
-	glBegin(GL_LINES);
-	for (i = 0, ep = Heliedges; i < Helinedges; i++, ep++)
+	glBegin(GL_TRIANGLES);
+	for (i = 0, tp = Helitris; i < Helintris; i++, tp++)
 	{
-		p0 = &Helipoints[ep->p0];
-		p1 = &Helipoints[ep->p1];
+		p0 = &Helipoints[tp->p0];
+		p1 = &Helipoints[tp->p1];
+		p2 = &Helipoints[tp->p2];
+
+		// fake "lighting" from above:
+
+		p01[0] = p1->x - p0->x;
+		p01[1] = p1->y - p0->y;
+		p01[2] = p1->z - p0->z;
+		p02[0] = p2->x - p0->x;
+		p02[1] = p2->y - p0->y;
+		p02[2] = p2->z - p0->z;
+		Cross(p01, p02, n);
+		Unit(n, n);
+		n[1] = fabs(n[1]);
+		n[1] += .25;
+		if (n[1] > 1.)
+			n[1] = 1.;
+		glColor3f(0., n[1], 0.);
+
 		glVertex3f(p0->x, p0->y, p0->z);
 		glVertex3f(p1->x, p1->y, p1->z);
+		glVertex3f(p2->x, p2->y, p2->z);
 	}
 	glEnd();
 	glPopMatrix();
